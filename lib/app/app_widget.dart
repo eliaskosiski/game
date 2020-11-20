34aef00a323c4f621a7742fa313528a364efe226
Elias Kosiski <elias_francisco_kosiski@hotmail.com>
2020-11-20 01:18:12 -0300

    atualização

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:app_jogo/app/pages/home/home_module.dart';
 
-
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
